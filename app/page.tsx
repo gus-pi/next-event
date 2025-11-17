e0fcee040c771f916fba77950d272818d355e84b
PieMenard <pinedo.m.gustavo@gmail.com>
2025-11-17 10:49:31 -0400

    add homepage and event cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import { events } from '@/lib/constants';
+import EventCard from './components/EventCard';
+import ExploreBtn from './components/ExploreBtn';
+
 const Page = () => {
     return (
         <section>
@@ -7,6 +11,17 @@ const Page = () => {
             <p className="text-center mt-5">
                 Hackatons, Meetups, and Conferences, ALl In One Place
             </p>
+            <ExploreBtn />
+            <div className="mt-20 space-y-7">
+                <h3>Fetured Events</h3>
+                <ul className="events">
+                    {events.map((event) => (
+                        <li key={event.title}>
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </section>
     );
 };
